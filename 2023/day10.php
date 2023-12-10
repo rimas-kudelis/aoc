3cbc2b9e9316bac74a0c9a5c7278399e9147c7c7
Rimas Kudelis <rimas@kudelis.lt>
2023-12-10 22:06:56 +0200

    Day 10 part 2

--- 2023/day10.php
+++ 2023/day10.php
@@ -11,6 +11,7 @@ const NE = 'L';
 const NW = 'J';
 const SW = '7';
 const SE = 'F';
+const BLANK = '.';
 const START = 'S';
 
 $fp = fopen(__DIR__ . '/input/day10.txt', 'r');
@@ -19,16 +20,16 @@ if (false === $fp) {
     throw new RuntimeException('Could not open input file!');
 }
 
-$startRow = $startCol = 0;
-$map = getMap($fp, $startRow, $startCol);
+$map = getMap($fp);
 
-$path = getPath($map, $startRow, $startCol);
+printf('Farthest point is %d steps away.' . PHP_EOL, calculatePathLengthInCleanMap($map) / 2);
+printf('There are %d tiles enclosed within the loop.' . PHP_EOL, countEnclosedTiles($map));
 
-printf('Farthest point is %d steps away.' . PHP_EOL, count($path) / 2);
-
-function getMap($fp, int &$startRow, int &$startCol): array
+function getMap($fp): array
 {
     $map = [];
+    $startRow = $startCol = null;
+
     while (false !== $line = fgets($fp)) {
         if ("\n" === $line) {
             continue;
@@ -45,31 +46,31 @@ function getMap($fp, int &$startRow, int &$startCol): array
 
     $map[$startRow][$startCol] = getStartingPipe($map, $startRow, $startCol);
 
-    return $map;
+    return cleanupMap($map, $startRow, $startCol);
 }
 
 function getStartingPipe(array $map, int $startRow, int $startCol): string
 {
     $north = $east = $south = $west = false;
 
-    if (in_array($map[$startRow - 1][$startCol] ?? '.', [SW, SE, NS], true)) {
+    if (in_array($map[$startRow - 1][$startCol] ?? BLANK, [SW, SE, NS], true)) {
         $north = true;
     }
 
-    if (in_array($map[$startRow][$startCol - 1] ?? '.', [NE, SE, EW], true)) {
+    if (in_array($map[$startRow][$startCol - 1] ?? BLANK, [NE, SE, EW], true)) {
         $west = true;
     }
 
-    if (in_array($map[$startRow][$startCol + 1] ?? '.', [NW, SW, EW], true)) {
+    if (in_array($map[$startRow][$startCol + 1] ?? BLANK, [NW, SW, EW], true)) {
         $east = true;
     }
 
-    if (in_array($map[$startRow + 1][$startCol] ?? '.', [NW, NE, NS], true)) {
+    if (in_array($map[$startRow + 1][$startCol] ?? BLANK, [NW, NE, NS], true)) {
         $south = true;
     }
 
-    if ((int)$north + (int)$south + (int)$west + (int)$east > 2) {
-        throw new RuntimeException('Too many pipes connecting to starting position!' . ((int)$north + (int)$south + (int)$west + (int)$east));
+    if ((int)$north + (int)$south + (int)$west + (int)$east !== 2) {
+        throw new RuntimeException('Unacceptable number of pipes connected to starting position!');
     }
 
     return match (true) {
@@ -79,49 +80,103 @@ function getStartingPipe(array $map, int $startRow, int $startCol): string
         $north && $west => NW,
         $north && $south => NS,
         $east && $west => EW,
-        default => throw new RuntimeException('Too few pipes connecting to starting position!'),
     };
 }
 
-function getPath(array $map, int $startRow, int $startCol): array
+function cleanupMap(array $map, int $startRow, int $startCol): array
 {
-    $path = [];
-    $cameFrom = null;
+    $cleanedMap = array_fill(
+        0,
+        count($map),
+        array_fill(0, count($map[0]), BLANK),
+    );
+
+    $cameFrom = match ($map[$startRow][$startCol]) {
+        NE, SE, EW => EAST,
+        NW, SW => WEST,
+        NS => NORTH,
+    };
+
     $currentRow = $startRow;
     $currentCol = $startCol;
 
     do {
         $currentPipe = $map[$currentRow][$currentCol];
-        $path[] = $currentPipe;
+        $cleanedMap[$currentRow][$currentCol] = $currentPipe;
 
         list ($currentRow, $currentCol, $cameFrom) = match ($currentPipe) {
             NE => match ($cameFrom) {
                 NORTH => [$currentRow, $currentCol + 1, WEST],
-                default => [$currentRow - 1, $currentCol, SOUTH],
+                EAST => [$currentRow - 1, $currentCol, SOUTH],
             },
             SE => match ($cameFrom) {
                 SOUTH => [$currentRow, $currentCol + 1, WEST],
-                default => [$currentRow + 1, $currentCol, NORTH],
+                EAST => [$currentRow + 1, $currentCol, NORTH],
             },
             NW => match ($cameFrom) {
                 NORTH => [$currentRow, $currentCol - 1, EAST],
-                default => [$currentRow - 1, $currentCol, SOUTH],
+                WEST => [$currentRow - 1, $currentCol, SOUTH],
             },
             SW => match ($cameFrom) {
                 SOUTH => [$currentRow, $currentCol - 1, EAST],
-                default => [$currentRow + 1, $currentCol, NORTH],
+                WEST => [$currentRow + 1, $currentCol, NORTH],
             },
             NS => match ($cameFrom) {
                 NORTH => [$currentRow + 1, $currentCol, NORTH],
-                default => [$currentRow - 1, $currentCol, SOUTH],
+                SOUTH => [$currentRow - 1, $currentCol, SOUTH],
             },
             EW => match ($cameFrom) {
+                EAST => [$currentRow, $currentCol - 1, EAST],
                 WEST => [$currentRow, $currentCol + 1, WEST],
-                default => [$currentRow, $currentCol - 1, EAST],
             },
-            default => throw new RuntimeException('Unexpected pipe: ' . $currentPipe),
         };
     } while ($currentRow !== $startRow || $currentCol !== $startCol);
 
-    return $path;
+    return $cleanedMap;
+}
+
+function calculatePathLengthInCleanMap(array $map): int
+{
+    $pathLength = 0;
+
+    foreach ($map as $row) {
+        $pathLength += count(array_filter($row, static fn(string $tile) => BLANK !== $tile));
+    }
+
+    return $pathLength;
+}
+
+function countEnclosedTiles(array $map): int
+{
+    $enclosedTiles = 0;
+
+    foreach ($map as $rowIndex => $row) {
+        foreach ($row as $columnIndex => $tile) {
+            if (isTileEnclosed($map, $rowIndex, $columnIndex)) {
+                $enclosedTiles++;
+            }
+        }
+    }
+
+    return $enclosedTiles;
+}
+
+function isTileEnclosed(array $map, int $row, int $col): bool
+{
+    if (BLANK !== $map[$row][$col]) {
+        return false;
+    }
+
+    // For a tile to be enclosed within a loop, the loop must have an uneven number of North-South passages to the East
+    // or West of it, or an uneven number of East-West passages to the North or South. These four indicators are
+    // mutually dependent on a clean map, so it's safe to check just one direction. I'll check for NS passages in East.
+    $easternTileCounts = array_count_values(array_slice($map[$row], $col + 1));
+
+    // This is not necessarily the correct amount of passages (pairs of NE and NW may be connected with each other
+    // directly or via EW tiles, thus not forming a North-South passage), but it can only be off by an even number,
+    // so it works fine for our purpose. The remaining NW and NE tiles will be matched by SE or SW tiles respectively,
+    // thus forming a North-South passage.
+    $passages = ($easternTileCounts[NS] ?? 0) + ($easternTileCounts[NW] ?? 0) + ($easternTileCounts[NE] ?? 0);
+
+    return 1 === $passages % 2;
 }
