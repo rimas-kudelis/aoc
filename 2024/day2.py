499ed961e97654d775bb84286ba791eff47878a9
Rimas Kudelis <rimas@kudelis.lt>
2024-12-02 20:50:55 +0200

    Dec 2 2024
    This took me a lot more than it should have. I had a bug which didn't pop up
    with the test data, nor with my modified test data. I ended up trying with the
    same (larger) `try_remove_indexes` definition for both checks, which,
    apparently, worked. When that worked, I figured out by comparing debug output
    that the problem was me not attempting removal of level #1 (0-based) when
    difference between level #1 and level #2 was bad, which meant that I missed
    some of the cases where level #1 was out of the "dynamic" of the report
    (e.g. [7, 4, 8, 10, 12]).

---
+++ 2024/day2.py
@@ -0,0 +1,97 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+from argparse import ArgumentParser
+from enum import Enum
+
+class ReportSafety(Enum):
+    SAFE = 0
+    SAFE_WHEN_DAMPENED = 1
+    UNSAFE = 2
+
+class ReportDynamic(Enum):
+    INCREASING = 0
+    DECREASING = 1
+
+parser = ArgumentParser(description='Analyze unusual data from reports for AoC 2024 day 2.')
+parser.add_argument('INPUT_FILE', help='Reports input file')
+args = parser.parse_args()
+
+def get_dynamic(level1, level2):
+    return ReportDynamic.INCREASING if level1 < level2 else ReportDynamic.DECREASING
+
+def safe_difference(level1, level2):
+    return 1 <= abs(level1 - level2) <= 3
+
+def analyze_report(report, enable_problem_dampener = True):
+    dynamic = None
+    for index, level in enumerate(report):
+        if index == 0:
+            continue
+
+        previous_level = report[index - 1]
+
+        # Any two adjacent levels differ by at least one and at most three.
+        if not safe_difference(previous_level, level):
+            if enable_problem_dampener:
+                # If this is the beginning of the report, try removing the previous element as well,
+                # because report level dynamic is not yet known for sure.
+                try_remove_indexes = [index - 1, index] if index <= 2 else [index]
+
+                for remove_index in try_remove_indexes:
+                    report_copy = report.copy()
+                    report_copy.pop(remove_index)
+                    if analyze_report(report_copy, False) == ReportSafety.SAFE:
+                        return ReportSafety.SAFE_WHEN_DAMPENED
+            return ReportSafety.UNSAFE
+
+        if index == 1:
+            dynamic = get_dynamic(previous_level, level)
+            continue
+
+        # Any two adjacent levels differ by at least one and at most three.
+        if dynamic != get_dynamic(previous_level, level):
+            if enable_problem_dampener:
+                # If we're at index 2, try removing previous elements as well,
+                # because report level dynamic is not yet known for sure.
+                try_remove_indexes = [0, 1, 2] if index == 2 else [index - 1, index]
+
+                for remove_index in try_remove_indexes:
+                    report_copy = report.copy()
+                    report_copy.pop(remove_index)
+                    if analyze_report(report_copy, False) == ReportSafety.SAFE:
+                        return ReportSafety.SAFE_WHEN_DAMPENED
+            return ReportSafety.UNSAFE
+
+    return ReportSafety.SAFE
+
+reports = []
+with open(args.INPUT_FILE) as f:
+    while True:
+        line = f.readline()
+        if not line:
+            break
+
+        line = line.strip()
+        if line == '':
+            continue
+
+        reports.append([int(report) for report in line.split(' ')])
+
+safe_reports = []
+dampened_safe_reports = []
+unsafe_reports = []
+for report in reports:
+    report_safety = analyze_report(report)
+
+    if report_safety == ReportSafety.SAFE:
+        safe_reports.append(report)
+    elif report_safety == ReportSafety.SAFE_WHEN_DAMPENED:
+        dampened_safe_reports.append(report)
+    else:
+        unsafe_reports.append(report)
+
+print('Safe reports:', len(safe_reports))
+print('Safe reports with problem dampener on:', len(dampened_safe_reports) + len(safe_reports))
+print('Unsafe reports:', len(unsafe_reports))
+
