63de07841689b968f82a44f14c072f4b3f5cb220
Rimas Kudelis <rimas@kudelis.lt>
2024-12-03 11:45:13 +0200

    Dec 3 2024, part 2

--- 2024/day3.py
+++ 2024/day3.py
@@ -3,27 +3,36 @@
 from argparse import ArgumentParser
 import re
 
-parser = ArgumentParser(description='Analyze unusual data from reports for AoC 2024 day 2.')
-parser.add_argument('INPUT_FILE', help='Reports input file')
-args = parser.parse_args()
-
 
 def find_instructions(memory):
-    return re.findall(r'mul\(\d{1,3},\d{1,3}\)', memory)
+    return re.findall(r'(mul\(\d{1,3},\d{1,3}\)|do\(\)|don\'t\(\))', memory)
 
 
-def execute(instruction):
+def multiply(instruction):
     multiplicants = re.findall(r'\d{1,3}', instruction)
     return int(multiplicants[0]) * int(multiplicants[1])
 
+
+parser = ArgumentParser(description='Salvage what\'s possible from program\'s corrupted memory for AoC 2024 day 3.')
+parser.add_argument('INPUT_FILE', help='Corrupted memory input file')
+args = parser.parse_args()
 
 with open(args.INPUT_FILE) as f:
     memory = f.read()
 
 instructions = find_instructions(memory)
-
-sum = 0
+original_sum = sum = 0
+do = True
 for instruction in instructions:
-    sum += execute(instruction)
+    if instruction.startswith('don\'t'):
+        do = False
+    elif instruction.startswith('do'):
+        do = True
+    else:
+        product = multiply(instruction)
+        original_sum += product
+        if do:
+            sum += product
 
-print('Sum of multiplications:', sum)
+print('Original sum of multiplications:', original_sum)
+print('More precise sum of multiplications:', sum)
