252bf9bb1ae0811974ab048e4d674c352bafa39c
Rimas Kudelis <rimas@kudelis.lt>
2024-12-05 11:31:58 +0200

    Simplify code
    This seems to be at the expense of some execution time. Not much, but still. No statistically significant tests performed though.

--- 2024/day5.py
+++ 2024/day5.py
@@ -26,23 +26,16 @@ def read_input(filename):
     return page_ordering_rules, manual_updates
 
 
-def count_correctly_ordered_updates(manual_updates, page_ordering_rules):
+def get_middle_page_number_sums(manual_updates, page_ordering_rules):
     correctly_ordered_middle_page_number_sum = incorrectly_ordered_middle_page_number_sum = 0;
 
     for update in manual_updates:
-        order_is_correct = True
-        for rule in page_ordering_rules:
-            if rule[0] not in update or rule[1] not in update:
-                continue
-            if update.index(rule[0]) > update.index(rule[1]):
-                order_is_correct = False
-                break
-        if order_is_correct:
-            middle_page_index = int((len(update) - 1) / 2)
+        fixed_update = reorder_pages(update, page_ordering_rules)
+        middle_page_index = int((len(update) - 1) / 2)
+
+        if fixed_update == update:
             correctly_ordered_middle_page_number_sum += update[middle_page_index]
         else:
-            fixed_update = reorder_pages(update, page_ordering_rules)
-            middle_page_index = int((len(fixed_update) - 1) / 2)
             incorrectly_ordered_middle_page_number_sum += fixed_update[middle_page_index]
 
     return correctly_ordered_middle_page_number_sum, incorrectly_ordered_middle_page_number_sum
@@ -50,10 +43,11 @@ def count_correctly_ordered_updates(manual_updates, page_ordering_rules):
 
 def reorder_pages(manual_update, page_ordering_rules):
     def compare(page1, page2):
-        for rule in page_ordering_rules:
-            if page1 in rule and page2 in rule:
-                return rule.index(page1) - rule.index(page2)
-        return 0
+        try:
+            rule = next(rule for rule in page_ordering_rules if page1 in rule and page2 in rule)
+            return rule.index(page1) - rule.index(page2)
+        except StopIteration:
+            return 0
 
     return sorted(manual_update, key=functools.cmp_to_key(compare))
 
@@ -63,7 +57,7 @@ parser.add_argument('INPUT_FILE', help='Update data')
 args = parser.parse_args()
 
 page_ordering_rules, manual_updates = read_input(args.INPUT_FILE)
-correct_sum, incorrect_sum = count_correctly_ordered_updates(manual_updates, page_ordering_rules)
+correct_sum, incorrect_sum = get_middle_page_number_sums(manual_updates, page_ordering_rules)
 
 print('Sum of correctly ordered update middle page numbers:', correct_sum)
 print('Sum of incorrectly ordered update middle page numbers:', incorrect_sum)
