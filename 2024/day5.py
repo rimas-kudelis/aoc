55e5a620841aac5cb83e0775eccea9639099a15e
Rimas Kudelis <rimas@kudelis.lt>
2024-12-05 07:59:22 +0200

    Dec 5 2024, part 2

--- 2024/day5.py
+++ 2024/day5.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 from argparse import ArgumentParser
+import functools
 
 
 def read_input(filename):
@@ -26,7 +27,7 @@ def read_input(filename):
 
 
 def count_correctly_ordered_updates(manual_updates, page_ordering_rules):
-    middle_page_number_sum = 0;
+    correctly_ordered_middle_page_number_sum = incorrectly_ordered_middle_page_number_sum = 0;
 
     for update in manual_updates:
         order_is_correct = True
@@ -37,10 +38,24 @@ def count_correctly_ordered_updates(manual_updates, page_ordering_rules):
                 order_is_correct = False
                 break
         if order_is_correct:
-            middle_page_index = int((len(update) - 1)  / 2)
-            middle_page_number_sum += update[middle_page_index]
+            middle_page_index = int((len(update) - 1) / 2)
+            correctly_ordered_middle_page_number_sum += update[middle_page_index]
+        else:
+            fixed_update = reorder_pages(update, page_ordering_rules)
+            middle_page_index = int((len(fixed_update) - 1) / 2)
+            incorrectly_ordered_middle_page_number_sum += fixed_update[middle_page_index]
 
-    return middle_page_number_sum
+    return correctly_ordered_middle_page_number_sum, incorrectly_ordered_middle_page_number_sum
+
+
+def reorder_pages(manual_update, page_ordering_rules):
+    def compare(page1, page2):
+        for rule in page_ordering_rules:
+            if page1 in rule and page2 in rule:
+                return rule.index(page1) - rule.index(page2)
+        return 0
+
+    return sorted(manual_update, key=functools.cmp_to_key(compare))
 
 
 parser = ArgumentParser(description='Sort out the Sleigh launch safety manual updates for AoC day 5.')
@@ -48,5 +63,7 @@ parser.add_argument('INPUT_FILE', help='Update data')
 args = parser.parse_args()
 
 page_ordering_rules, manual_updates = read_input(args.INPUT_FILE)
+correct_sum, incorrect_sum = count_correctly_ordered_updates(manual_updates, page_ordering_rules)
 
-print('Sum of correctly ordered update middle page numbers:', count_correctly_ordered_updates(manual_updates, page_ordering_rules))
+print('Sum of correctly ordered update middle page numbers:', correct_sum)
+print('Sum of incorrectly ordered update middle page numbers:', incorrect_sum)
