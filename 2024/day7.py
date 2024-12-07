42004e3f3fe7f339b7f9104413620322e882eef8
Rimas Kudelis <rimas@kudelis.lt>
2024-12-07 21:50:17 +0200

    Dec 7 2024, part 2
    Ohhh, I'm sooo smart to have done the calculation backwards right from the start!
    At first I thought this mess up part two, but then I realised that it actually helps a lot with part2 instead!

--- 2024/day7.py
+++ 2024/day7.py
@@ -14,45 +14,42 @@ def read_input(filename):
     return instructions
 
 
-def is_instruction_potentially_valid(test_value, operands):
-    # No zeroes in my input
-    # try:
-    #     if zero_index := operands.index(0):
-    #         if is_instruction_potentially_valid(test_value, operands[zero_index + 1:]):
-    #             return True
-    #         operands.pop(zero_index)
-    #         if is_instruction_potentially_valid(test_value, operands.copy()):
-    #             return True
-    #         return False
-    # except ValueError:
-    #     pass
-
-    # No negative numbers in my input
+def is_equation_potentially_valid(test_value, operands, try_concatenate):
+    # Note: no zeroes or negative numbers are expected in input
     if test_value == sum(operands): return True
 
-    if len(operands) == 2: return test_value == operands[0] * operands[1]
+    if len(operands) == 2:
+        return test_value == operands[0] * operands[1] or str(test_value) == str(operands[0]) + str(operands[1])
 
     current_operand = operands.pop()
-    if is_instruction_potentially_valid(test_value - current_operand, operands.copy()): return True
+    if is_equation_potentially_valid(test_value - current_operand, operands.copy(), try_concatenate):
+        return True
 
-    if test_value % current_operand == 0 and is_instruction_potentially_valid(test_value // current_operand, operands.copy()): return True
+    if test_value % current_operand == 0 and is_equation_potentially_valid(test_value // current_operand, operands.copy(), try_concatenate):
+        return True
+
+    if try_concatenate:
+        test_value, current_operand = str(test_value), str(current_operand)
+        if test_value.endswith(current_operand) and is_equation_potentially_valid(int(test_value.removesuffix(current_operand)), operands.copy(), try_concatenate):
+            return True
 
     return False
 
 
-def get_total_calibration_result(instructions):
+def get_total_calibration_result(instructions, try_concatenate):
     total_calibration_result = 0
 
     for test_value, operands in instructions:
-        if is_instruction_potentially_valid(test_value, operands):
+        if is_equation_potentially_valid(test_value, operands.copy(), try_concatenate):
             total_calibration_result += test_value
 
     return total_calibration_result
 
 
-parser = ArgumentParser(description='Find possibly correct calibration instructions for AoC day 7.')
-parser.add_argument('INPUT_FILE', help='Calibration instructions file')
+parser = ArgumentParser(description='Find possibly correct calibration equations for AoC day 7.')
+parser.add_argument('INPUT_FILE', help='Calibration equations file')
 args = parser.parse_args()
 
 instructions = read_input(args.INPUT_FILE)
-print('Total calibration result:', get_total_calibration_result(instructions))
+print('Total calibration result without concatenation:', get_total_calibration_result(instructions, False))
+print('Total calibration result with concatenation:', get_total_calibration_result(instructions, True))
