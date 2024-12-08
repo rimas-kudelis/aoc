7c9f9896026d3fc2cea1de5fd01c20b4ea63ada9
Rimas Kudelis <rimas@kudelis.lt>
2024-12-08 10:47:52 +0200

    Dec 8 2024, part 2

--- 2024/day8.py
+++ 2024/day8.py
@@ -23,7 +23,7 @@ def collect_antennas_by_frequency(map):
     return antennas
 
 
-def count_antinodes(area_map):
+def count_antinodes(area_map, account_for_resonant_harmonics=False):
     map_rows = len(area_map)
     map_cols = len(area_map[0])
     antinodes = set()
@@ -31,16 +31,27 @@ def count_antinodes(area_map):
 
     for frequency in antennas:
         for antenna1_index, antenna1 in enumerate(antennas[frequency]):
-            for antenna2_index, antenna2 in enumerate(antennas[frequency][antenna1_index + 1:]):
+            for antenna2 in antennas[frequency][antenna1_index + 1:]:
                 row_diff = antenna2[0] - antenna1[0]
                 col_diff = antenna2[1] - antenna1[1]
-                potential_antinodes = [
-                    (antenna1[0] - row_diff, antenna1[1] - col_diff),
-                    (antenna2[0] + row_diff, antenna2[1] + col_diff),
-                ]
-                for antinode in potential_antinodes:
-                    if 0 <= antinode[0] < map_rows and 0 <= antinode[1] < map_cols:
+                if not account_for_resonant_harmonics:
+                    potential_antinodes = {
+                        (antenna1[0] - row_diff, antenna1[1] - col_diff),
+                        (antenna2[0] + row_diff, antenna2[1] + col_diff),
+                    }
+                    for antinode in potential_antinodes:
+                        if 0 <= antinode[0] < map_rows and 0 <= antinode[1] < map_cols:
+                            antinodes.add(antinode)
+                else:
+                    antinode = antenna1
+                    while 0 <= antinode[0] < map_rows and 0 <= antinode[1] < map_cols:
                         antinodes.add(antinode)
+                        antinode = (antinode[0] - row_diff, antinode[1] - col_diff)
+                    antinode = antenna2
+                    while 0 <= antinode[0] < map_rows and 0 <= antinode[1] < map_cols:
+                        antinodes.add(antinode)
+                        antinode = (antinode[0] + row_diff, antinode[1] + col_diff)
+
     return len(antinodes)
 
 
@@ -50,4 +61,5 @@ args = parser.parse_args()
 
 area_map = read_map(args.INPUT_FILE)
 
-print('Number of antinodes is:', count_antinodes(area_map))
+print('Initially calculated number of antinodes is:', count_antinodes(area_map, False))
+print('Corrected number of antinodes is:', count_antinodes(area_map, True))
