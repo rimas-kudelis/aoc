7b0fafe27fe0ee65e84418f82f5722d156df47d9
Rimas Kudelis <rimas@kudelis.lt>
2024-12-09 11:39:26 +0200

    Dec 9 2024, part 2

--- 2024/day9.py
+++ 2024/day9.py
@@ -10,7 +10,7 @@ def read_disk_map(filename):
     return disk_map
 
 
-def compact(disk_map):
+def compact_with_fragmentation(disk_map):
     compacted_map = []
     for current_region_index, current_region in enumerate(disk_map):
         if current_region_index % 2 == 0:
@@ -21,7 +21,7 @@ def compact(disk_map):
             # free space
             while True:
                 end_region_index = len(disk_map) - 1
-                if end_region_index %2 != 0:
+                if end_region_index % 2 != 0:
                     # end region is free space
                     disk_map.pop()
                     continue
@@ -40,18 +40,77 @@ def compact(disk_map):
     return compacted_map
 
 
+def compact_without_fragmentation(disk_map):
+    verbose_map = build_verbose_map(disk_map)
+    moved_something = True
+
+    while moved_something:
+        moved_something = False
+
+        for file_region_index in range(len(verbose_map) - 1, 0, -1):
+            file_region = verbose_map[file_region_index]
+
+            if not file_region['can_be_moved']:
+                continue
+
+            for target_region_index in range(0, file_region_index):
+                target_region = verbose_map[target_region_index]
+
+                if target_region['file_index'] is not None or target_region['size'] < file_region['size']:
+                    continue
+
+                if (size_difference := target_region['size'] - file_region['size']) > 0:
+                    verbose_map.insert(
+                        target_region_index + 1,
+                        {'size': size_difference, 'can_be_moved': False, 'file_index': None},
+                    )
+                    target_region['size'] = file_region['size']
+
+                target_region['file_index'] = file_region['file_index']
+                file_region['file_index'] = None
+                moved_something = True
+                break
+
+            file_region['can_be_moved'] = False
+
+    return build_compacted_map_from_verbose_map(verbose_map)
+
+
+def build_verbose_map(disk_map):
+    return [
+        {
+            'size': region_size,
+            'can_be_moved': region_index % 2 == 0,
+            'file_index': region_index // 2 if region_index % 2 == 0 else None,
+        }
+        for region_index, region_size in enumerate(disk_map)
+    ]
+
+
+def build_compacted_map_from_verbose_map(verbose_map):
+    compacted_map = []
+
+    for region in verbose_map:
+        for i in range(region['size']):
+            compacted_map.append(region['file_index'])
+
+    return compacted_map
+
+
 def get_checksum(disk_map):
     checksum = 0
     for block_index, file_id in enumerate(disk_map):
-        checksum += block_index * file_id
+        if file_id is not None:
+            checksum += block_index * file_id
 
     return checksum
+
 
 parser = ArgumentParser(description='Compact amphipod\'s hard drive and calculate its checksum for AoC day 9.')
 parser.add_argument('INPUT_FILE', help='Disk map file')
 args = parser.parse_args()
 
 disk_map = read_disk_map(args.INPUT_FILE)
-compacted_map = compact(disk_map.copy())
 
-print('Compacted filesystem checksum:', get_checksum(compacted_map))
+print('Compacted fragmented filesystem checksum:', get_checksum(compact_with_fragmentation(disk_map.copy())))
+print('Compacted non-fragmented filesystem checksum:', get_checksum(compact_without_fragmentation(disk_map)))
