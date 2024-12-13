39252234061781b339e707271b3fdfb2e05487f9
Rimas Kudelis <rimas@kudelis.lt>
2024-12-13 10:27:20 +0200

    Dec 13 2024

---
+++ 2024/day13.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python3
+
+from argparse import ArgumentParser
+import re
+import numpy
+
+
+def read_claw_machine_configuration(filename):
+    with open(filename) as f:
+        lines = [line.strip() for line in f if line.strip() != '']
+
+    machines = []
+    for line_num in range(0, len(lines), 3):
+        (ax, ay) = [int(move) for move in re.match(r'^Button A: X([+-]\d+), Y([+-]\d+)$', lines[line_num]).groups()]
+        (bx, by) = [int(move) for move in re.match(r'^Button B: X([+-]\d+), Y([+-]\d+)$', lines[line_num + 1]).groups()]
+        (px, py) = [int(pos) for pos in re.match(r'^Prize: X=([+-]?\d+), Y=([+-]?\d+)$', lines[line_num + 2]).groups()]
+
+        machines.append({'ax': ax, 'ay': ay, 'bx': bx, 'by': by, 'px': px, 'py': py})
+
+    return machines
+
+
+def calculate_minimum_tokens_to_win(claw_machines, fix_calculation=False):
+    tokens = tokens_corrected = 0
+
+    for machine in claw_machines:
+        if solution := solve_machine(machine, fix_calculation):
+            tokens += 3 * solution['a'] + solution['b']
+
+    return tokens
+
+
+def solve_machine(machine, fix_calculation):
+    left = numpy.array([[machine['ax'], machine['bx']], [machine['ay'], machine['by']]])
+    right = numpy.array([10000000000000 + machine['px'], 10000000000000 + machine['py']]) if fix_calculation \
+        else numpy.array([machine['px'], machine['py']])
+    (a, b) = numpy.round(numpy.linalg.inv(left).dot(right), 4)
+
+    # Somehow numpy returns quite significantly imprecise floats. 
+    if abs(int(round(a)) - a) <= 0.0001 and abs(int(round(b)) - b) <= 0.0001:  # and a <= 100 and b <= 100:
+        return {'a': int(round(a)), 'b': int(round(b))}
+
+    return None
+
+
+parser = ArgumentParser(description='Calculate tokens required to win all prices for AoC day 13.')
+parser.add_argument('INPUT_FILE', help='Claw machine description file')
+args = parser.parse_args()
+
+claw_machines = read_claw_machine_configuration(args.INPUT_FILE)
+print('Need {0} tokens to win all possible prizes.'.format(calculate_minimum_tokens_to_win(claw_machines)))
+print('Need {0} tokens to win all possible prizes after reviewing the measurements.'.format(
+    calculate_minimum_tokens_to_win(claw_machines, True)))
