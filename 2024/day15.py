5b212fed3082bc0a1c647f343be99ba791418659
Rimas Kudelis <rimas@kudelis.lt>
2024-12-16 23:12:35 +0200

    Dec 15 2024, part 2

--- 2024/day15.py
+++ 2024/day15.py
@@ -8,6 +8,15 @@ class Tile:
     BOX = 'O'
     BLANK = '.'
     ROBOT = '@'
+    BIG_BOX_L = '['
+    BIG_BOX_R = ']'
+
+
+class Movement:
+    UP = '^'
+    DOWN = 'v'
+    LEFT = '<'
+    RIGHT = '>'
 
 
 def read_input(filename):
@@ -38,77 +47,94 @@ def read_input(filename):
     return warehouse_map, movements, robot_position
 
 
+def scale_warehouse(warehouse_map, robot_position):
+    scaled_warehouse = []
+    for row in warehouse_map:
+        scaled_row = []
+        for tile in row:
+            match tile:
+                case Tile.BOX:
+                    scaled_row.extend([Tile.BIG_BOX_L, Tile.BIG_BOX_R])
+                case Tile.WALL:
+                    scaled_row.extend([Tile.WALL, Tile.WALL])
+                case Tile.BLANK:
+                    scaled_row.extend([Tile.BLANK, Tile.BLANK])
+        scaled_warehouse.append(scaled_row)
+
+    return scaled_warehouse, (robot_position[0], robot_position[1] * 2)
+
+
 def simulate_robot_movements(warehouse_map, robot_movements, robot_position):
     for movement in robot_movements:
         robot_position = move_if_possible(warehouse_map, robot_position, movement)
 
 
 def move_if_possible(warehouse_map, robot_position, movement):
-    (row, col) = robot_position
-    blank_tile = new_position = None
+    move_tiles, new_robot_position = get_movement_result(warehouse_map, robot_position, movement)
+    if move_tiles is not None:
+        for move_group in move_tiles:
+            for (src, dest) in move_group:
+                warehouse_map[dest[0]][dest[1]] = warehouse_map[src[0]][src[1]]
+                warehouse_map[src[0]][src[1]] = Tile.BLANK
 
+    return new_robot_position
+
+
+def get_movement_result(warehouse_map, object_position, movement):
+    (row, col) = object_position
     match movement:
-        case '^':
-            new_position = (row - 1, col)
-            for try_row in range(row - 1, 0, -1):
-                match warehouse_map[try_row][col]:
-                    case Tile.BOX:
-                        continue
-                    case Tile.WALL:
-                        break
-                    case Tile.BLANK:
-                        blank_tile = (try_row, col)
-                        break
-                    case _:
-                        raise ValueError('Unexpected map tile: {}'.format(warehouse_map[try_row][col]))
-        case '<':
-            new_position = (row, col - 1)
-            for try_col in range(col - 1, 0, -1):
-                match warehouse_map[row][try_col]:
-                    case Tile.BOX:
-                        continue
-                    case Tile.WALL:
-                        break
-                    case Tile.BLANK:
-                        blank_tile = (row, try_col)
-                        break
-                    case _:
-                        raise ValueError('Unexpected map tile: {}'.format(warehouse_map[row][try_col]))
-        case 'v':
-            new_position = (row + 1, col)
-            for try_row in range(row + 1, len(warehouse_map) - 1, 1):
-                match warehouse_map[try_row][col]:
-                    case Tile.BOX:
-                        continue
-                    case Tile.WALL:
-                        break
-                    case Tile.BLANK:
-                        blank_tile = (try_row, col)
-                        break
-                    case _:
-                        raise ValueError('Unexpected map tile: {}'.format(warehouse_map[try_row][col]))
-        case '>':
-            new_position = (row, col + 1)
-            for try_col in range(col + 1, len(warehouse_map[row]) - 1, 1):
-                match warehouse_map[row][try_col]:
-                    case Tile.BOX:
-                        continue
-                    case Tile.WALL:
-                        break
-                    case Tile.BLANK:
-                        blank_tile = (row, try_col)
-                        break
-                    case _:
-                        raise ValueError('Unexpected map tile: {}'.format(warehouse_map[row][try_col]))
+        case Movement.UP:
+            target_position = (row - 1, col)
+        case Movement.DOWN:
+            target_position = (row + 1, col)
+        case Movement.LEFT:
+            target_position = (row, col - 1)
+        case Movement.RIGHT:
+            target_position = (row, col + 1)
         case _:
             raise ValueError('Unexpected robot movement: {}'.format(movement))
 
-    if blank_tile is not None:
-        warehouse_map[blank_tile[0]][blank_tile[1]] = Tile.BOX
-        warehouse_map[new_position[0]][new_position[1]] = Tile.BLANK
-        return new_position
+    target_tile = warehouse_map[target_position[0]][target_position[1]]
+    if target_tile == Tile.BLANK:
+        return [{(object_position, target_position)}], target_position
 
-    return robot_position
+    if target_tile == Tile.WALL:
+        return None, object_position
+
+    if target_tile == Tile.BOX \
+            or target_tile in [Tile.BIG_BOX_L, Tile.BIG_BOX_R] and movement in [Movement.LEFT, Movement.RIGHT]:
+
+        moves = get_movement_result(warehouse_map, target_position, movement)[0]
+        if moves is None:
+            return None, object_position
+
+        moves.append({(object_position, target_position)})
+        return moves, target_position
+
+    if target_tile in [Tile.BIG_BOX_L, Tile.BIG_BOX_R]:
+        target_twin_position = (target_position[0], target_position[1] + 1) if target_tile == Tile.BIG_BOX_L \
+            else (target_position[0], target_position[1] - 1)
+
+        moves = get_movement_result(warehouse_map, target_position, movement)[0]
+        if moves is None:
+            return None, object_position
+
+        moves_twin = get_movement_result(warehouse_map, target_twin_position, movement)[0]
+        if moves_twin is None:
+            return None, object_position
+
+        priority_delta = len(moves) - len(moves_twin)
+        if priority_delta < 0:
+            moves, moves_twin = moves_twin, moves
+            priority_delta *= -1
+
+        for priority in range(0, len(moves_twin)):
+            moves[priority + priority_delta] = moves[priority + priority_delta] | moves_twin[priority]
+
+        moves.append({(object_position, target_position)})
+        return moves, target_position
+
+    raise ValueError('Unexpected tile: {}'.format(target_tile))
 
 
 def get_sum_of_gps_coordinates(warehouse_map):
@@ -116,14 +142,17 @@ def get_sum_of_gps_coordinates(warehouse_map):
 
     for row_index, row in enumerate(warehouse_map):
         for tile_index, tile in enumerate(row):
-            if tile == Tile.BOX:
+            if tile in (Tile.BOX, Tile.BIG_BOX_L):
                 sum += row_index * 100 + tile_index
 
     return sum
 
 
-def print_map(warehouse_map):
-    for row in warehouse_map:
+def print_map(warehouse_map, robot_position=None):
+    for row_index, row in enumerate(warehouse_map):
+        row = row.copy()
+        if robot_position is not None and row_index == robot_position[0]:
+            row[robot_position[1]] = Tile.ROBOT
         print(''.join(row))
 
 
@@ -132,9 +161,10 @@ parser.add_argument('INPUT_FILE', help='Warehouse map and robot movements file.'
 args = parser.parse_args()
 
 warehouse_map, robot_movements, robot_position = read_input(args.INPUT_FILE)
+scaled_warehouse_map, scaled_robot_position = scale_warehouse(warehouse_map, robot_position)
 
-# print_map(warehouse_map)
-# print(robot_movements, robot_position)
 simulate_robot_movements(warehouse_map, robot_movements, robot_position)
-print('Sum of GPS coordinates:', get_sum_of_gps_coordinates(warehouse_map))
-# print_map(warehouse_map)
+print('Sum of GPS coordinates for the smaller warehouse:', get_sum_of_gps_coordinates(warehouse_map))
+
+simulate_robot_movements(scaled_warehouse_map, robot_movements, scaled_robot_position)
+print('Sum of GPS coordinates for the bigger warehouse:', get_sum_of_gps_coordinates(scaled_warehouse_map))
